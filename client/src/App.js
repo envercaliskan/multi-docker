1841797e5a788b345160e18bee16081b3acae40a
Enver Caliskan <envercaliskan@outlook.com>
2023-06-14 14:09:47 +0400

    init

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+      </div>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Fib />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
